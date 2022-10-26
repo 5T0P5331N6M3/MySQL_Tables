783c3e6e90922078b69b561854c628a2bbb1686b
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-26 15:11:47 -0500

    Completed MySQL Limit Exercises.

---
+++ limit_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
+
+/*SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name LIKE 'M%'
+LIMIT 25 OFFSET 50;*/
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
+
+
