9e23c8e2fa609a66c44682588d0dabb0688818cc
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-27 09:28:48 -0500

    Initial GROUP BY MySQL Exercise, Codeup.

---
+++ src/group_by_exercises.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM employees;
+
+/*In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
+
+
+Senior Engineer
+Staff
+Engineer
+Senior Staff
+Assistant Engineer
+Technique Leader
+Manager*/
