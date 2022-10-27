e1b05d8895b35fad130d6b8e188da5cdecf5270c
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-27 10:16:15 -0500

    Completed Number 6. Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others. 189 Chleq 190 Lindqvist 168 Qiwen

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -6,3 +6,10 @@ FROM titles;
 SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name, first_name;
 
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT last_name, COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY COUNT(*);
+
+SELECT first_name, COUNT(first_name)
+FROM employees
+WHERE first_name NOT LIKE '%a%'
+GROUP BY first_name;
