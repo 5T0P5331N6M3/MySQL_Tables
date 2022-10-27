97bfc405629f836fd925041b4c8a2b651d4675d0
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-27 10:44:22 -0500

    Refactored exercises during instructor review.

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -9,6 +9,6 @@ SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_n
 
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
-SELECT last_name, COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY COUNT(*);
+SELECT last_name, COUNT(last_name) AS COUNT FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY COUNT(last_name);
 
-SELECT gender, COUNT(*)FROM employees WHERE gender AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender;
+SELECT gender, COUNT(gender) AS gender_count FROM employees WHERE gender AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender;
