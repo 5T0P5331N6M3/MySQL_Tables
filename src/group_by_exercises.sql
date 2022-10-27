d72032d2efdfc7ff90d641adc8e570a4fc0fde06
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-27 10:07:20 -0500

    Completed Number 5.

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -4,3 +4,5 @@ SELECT DISTINCT title
 FROM titles;
 
 SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name, first_name;
+
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
